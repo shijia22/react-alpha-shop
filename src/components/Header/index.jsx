b1b390262289437d58c20959e58eea7976f26f04
shijia22 <shichia0207@gmail.com>
2022-07-13 21:25:17 +0800

    feat: 🎸 add header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className="p-4 border-b-4">
+      <ul class="flex">
+      <li className="mr-5">男款</li>
+      <li className="mr-5">女款</li>
+      <li className="mr-5">最新消息</li>
+      <li className="mr-5">客製商品</li>
+      <li className="mr-5">聯絡我們</li>
+      </ul>
+    </header>
+  );
+};
+export default Header;
