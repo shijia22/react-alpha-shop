61f528a24708f70ae4f8a0869dcdc4f9f9686474
shijia22 <shichia0207@gmail.com>
2022-07-13 22:33:55 +0800

    feat: 🎸 Cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import StepProgress from '../src/components/StepProgress';
 import Header from '../src/components/Header'
 import ProgressControl from '../src/components/ProgressControl';
+import Cart from '../src/components/Cart';
 
 function App() {
 
@@ -10,12 +11,13 @@ function App() {
     <div classNameName="px-40 max-w-[1440px] min-h-screen">
       <Header />
       <h1 classNameName="mb-12">結帳</h1>
-      <div className="flex flex-col">
-      <StepProgress />
+      <StepProgress/>
+      <div className="flex">
         <div className="flex-grow mr-[8.125rem]">
           <ProgressControl />
         </div>
         <div className="w-2/5">
+          <Cart />
         </div>
       </div>
     </div>
---
+++ src/components/Cart/index.jsx
@@ -0,0 +1,44 @@
+import { React } from 'react';
+
+const Cart = () => {
+  return (
+    <div className="container py-8 px-6 border divide-[#f0f0f5] rounded-lg">
+      <h2 className="mb-8">購物籃</h2>
+      <div
+        className="mb-8 flex justify-between">
+        <img alt="" src='https://fakeimg.pl/300x300/' className='w-[100px] h-[100px]'/>
+        <div className="items-center min-w-[200px]">
+          <span>貓貓罐罐</span>
+          <div className="flex justify-between mt-4">
+            <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">-</button>
+            <span>12</span>
+            <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">+</button>
+          </div>
+        </div >
+        <div>$ 1000</div>
+      </div>
+      <div
+        className="mb-8 flex justify-between">
+        <img alt="" src='https://fakeimg.pl/300x300/' className='w-[100px] h-[100px]' />
+        <div className="items-center min-w-[200px]">
+          <span>貓貓肉乾</span>
+          <div className="flex justify-between mt-4">
+            <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">-</button>
+            <span>2</span>
+            <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">+</button>
+          </div>
+        </div >
+        <div>$ 800</div>
+      </div>
+      <div className="pt-5 pb-8 my-4 flex justify-between">
+        <span>運費</span>
+        <span>$ 500</span>
+      </div>
+      <div className="my-4 flex justify-between">
+        <span>小計</span>
+        <span>$ 5000</span>
+      </div>
+    </div >
+  );
+};
+export default Cart;
