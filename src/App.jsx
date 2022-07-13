fc4c9ac897311bbcfabc588a14275619a07b9336
shijia22 <shichia0207@gmail.com>
2022-07-13 21:25:37 +0800

    feat: 🎸 add main container

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import React, { useState } from 'react'
 import './App.css'
 import StepProgress from '../src/components/StepProgress';
 import Header from '../src/components/Header'
+import ProgressControl from '../src/components/ProgressControl';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="text-center">
-      <Header/>
-      <header className="min-h-screen text-white flex flex-col justify-center items-center">
-        <StepProgress />
-      </header>
+    <div classNameName="px-40 max-w-[1440px] min-h-screen">
+      <Header />
+      <h1 classNameName="mb-12">結帳</h1>
+      <div className="flex">
+        <div className="flex-grow mr-[8.125rem]">
+        </div>
+        <div className="w-2/5">
+          <ProgressControl />
+        </div>
+      </div>
+      <StepProgress />
     </div>
   )
 }
