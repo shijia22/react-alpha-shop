356ff5b8e81f8840a18a170929f7e56b3baf48d3
shijia22 <shichia0207@gmail.com>
2022-07-14 05:57:02 +0800

    feat: 🎸 Step1

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import StepProgress from '../src/components/StepProgress';
 import Header from '../src/components/Header'
 import ProgressControl from '../src/components/ProgressControl';
 import Cart from '../src/components/Cart';
+import Step1 from '../src/components/Step1';
 
 function App() {
 
   return (
-    <div classNameName="px-40 max-w-[1440px] min-h-screen">
+    <div className="px-40 max-w-[1440px] min-h-screen">
       <Header />
-      <h1 classNameName="mb-12">結帳</h1>
+      <h1 className="mb-12">結帳</h1>
       <StepProgress/>
       <div className="flex">
         <div className="flex-grow mr-[8.125rem]">
+          <Step1 />
           <ProgressControl />
         </div>
         <div className="w-2/5">
