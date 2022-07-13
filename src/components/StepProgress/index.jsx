3abd290c28df424240475554fb7d6ca83eec758b
shijia22 <shichia0207@gmail.com>
2022-07-13 09:21:11 +0800

    feat: 🎸 add StepProgress - notyet

---
+++ src/components/StepProgress/index.jsx
@@ -0,0 +1,26 @@
+import './style.css';
+import { React } from 'react';
+
+const StepProgress = () => {
+  return (
+  <div className="mb-16 flex justify-between items-center">
+    <div className="progress-text flex items-center">
+      <span className="circle flex justify-center mr-2 items-center"
+        ></span>
+      寄送地址
+    </div>
+    <div className="line"></div>
+    <div className="progress-text flex items-center">
+      <span className="circle flex justify-center mr-2 items-center"
+        ></span>
+      運送方式
+    </div>
+    <div className="line"></div>
+    <div className="progress-text flex items-center">
+      <span className="circle flex justify-center mr-2 items-center"></span>
+      付款資訊
+    </div>
+  </div>
+  );
+};
+export default StepProgress;
