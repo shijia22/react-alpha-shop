5b01628986153adf0e6e9c97510c9788acc3eb15
shijia22 <shichia0207@gmail.com>
2022-07-14 08:35:04 +0800

    style: 💄 change ProgressControl

--- src/components/ProgressControl/index.jsx
+++ src/components/ProgressControl/index.jsx
@@ -4,15 +4,15 @@ const ProgressControl = () => {
   return (
     <div className="pt-6 flex justify-between border-t">
       <button
-        className="back flex items-center">
+        className="back flex items-center py-4 px-8">
         {/* <img src="../images/left.png" alt="" className="w-6 h-6 mr-3" /> */}
         上一步</button>
       <button
-        className="flex items-center py-4 px-3 rounded-lg bg-[#f67599]">
+        className="flex items-center py-4 px-8 rounded-lg bg-[#f67599] text-white">
         下一步
         {/* <img src="../images/right.png" alt="" className="w-6 h-6 mr-3" /> */}
       </button>
-      {/* <button className="flex items-center py-4 px-3 rounded-lg bg-[#f67599]">確認訂單</button> */}
+      {/* <button className="flex items-center py-4 px-8 rounded-lg bg-[#f67599]">確認訂單</button> */}
     </div>
   );
 };
