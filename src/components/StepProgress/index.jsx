f8c91098f28e096612c88ab27544424566f94d32
shijia22 <shichia0207@gmail.com>
2022-07-22 21:55:21 +0800

    fix: 🐛 StepProgress active & checked css

--- src/components/StepProgress/index.jsx
+++ src/components/StepProgress/index.jsx
@@ -10,16 +10,16 @@ const StepProgress: React.FC<StepProgressProps> = ({ step }) => {
   return (
     <div className="mb-16 flex justify-between items-center w-1/2">
       <div className={`progress-text flex items-center ${step === 1 ? 'active' : 'checked'}`}>
-        <span className="circle flex justify-center mr-2 items-center" />
+        <span className={`circle flex justify-center mr-2 items-center ${step === 1 ? 'active' : 'checked'}`} />
         寄送地址
       </div>
-      <div className={`line ${step === 1 ? '' : 'active'}`} />
-      <div className={`progress-text flex items-center ${step <= 2 ? 'bg-white text-gray-600' : 'checked'}`}>
-        <span className="circle flex justify-center mr-2 items-center" />
+      <div className="line active" />
+      <div className={`progress-text flex items-center ${step <= 2 ? 'active' : 'checked'}`}>
+        <span className={`circle flex justify-center mr-2 items-center ${step <= 2 ? 'active' : 'checked'}`} />
         運送方式
       </div>
-      <div className={`line ${step <= 2 ? '' : 'active'}`} />
-      <div className={`progress-text flex items-center ${step === 3 ? 'bg-white text-gray-600' : ''}`}>
+      <div className={`line ${step >= 2 ? 'active' : ''}`} />
+      <div className={`progress-text flex items-center ${step === 3 ? 'active' : ''}`}>
         <span className="circle flex justify-center mr-2 items-center" />
         付款資訊
       </div>
