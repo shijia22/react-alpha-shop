c3a76c41655a8c78efb352d0466f825ab19c3826
shijia22 <shichia0207@gmail.com>
2022-07-24 20:58:57 +0800

    refactor: 💡 add for loop Lineitem

--- src/components/Cart/Lineitem/index.jsx
+++ src/components/Cart/Lineitem/index.jsx
@@ -1,33 +1,45 @@
 import { React } from 'react';
 
 const LineItem = () => {
+  const products = [
+    {
+      id: '1',
+      name: '貓咪罐罐',
+      img: 'https://picsum.photos/300/300?text=1',
+      price: 100,
+      quantity: 2,
+    },
+    {
+      id: '2',
+      name: '貓咪干干',
+      img: 'https://picsum.photos/300/300?text=2',
+      price: 200,
+      quantity: 1,
+    },
+  ];
+
+  const list = []
+
+  for (const [i, product] of products.entries()) {
+    list.push(<div className="mb-8 flex justify-between">
+      <img alt="" src={product.img} className='w-[100px] h-[100px]' />
+      <div className="items-center min-w-[100px]">
+        <span>{product.name}</span>
+        <div className="flex justify-between mt-4">
+          <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">-</button>
+          <span>{product.quantity}</span>
+          <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">+</button>
+        </div>
+      </div >
+      <div>$ {product.price}</div>
+    </div>)
+  }
+
   return (
     <>
-      <div className="mb-8 flex justify-between">
-        <img alt="" src='https://fakeimg.pl/300x300/' className='w-[100px] h-[100px]' />
-        <div className="items-center min-w-[100px]">
-          <span>貓貓罐罐</span>
-          <div className="flex justify-between mt-4">
-            <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">-</button>
-            <span>12</span>
-            <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">+</button>
-          </div>
-        </div >
-        <div>$ 1000</div>
-      </div>
-      <div className="mb-8 flex justify-between">
-        <img alt="" src='https://fakeimg.pl/300x300/' className='w-[100px] h-[100px]' />
-        <div className="items-center min-w-[100px]">
-          <span>貓貓肉乾</span>
-          <div className="flex justify-between mt-4">
-            <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">-</button>
-            <span>2</span>
-            <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">+</button>
-          </div>
-        </div >
-        <div>$ 800</div>
-      </div>
+      {list}
     </>
-  );
-};
+  )
+}
+
 export default LineItem;
