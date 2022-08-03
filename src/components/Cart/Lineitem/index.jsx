bdd7fc491f97e1153fb38a624987554d0ff463ca
shijia22 <shichia0207@gmail.com>
2022-08-03 21:21:33 +0800

    feat: 🎸 CreateContext & listItems data

--- src/components/Cart/Lineitem/index.jsx
+++ src/components/Cart/Lineitem/index.jsx
@@ -1,44 +1,21 @@
 import { React } from 'react';
 
-const ListItem = () => {
-  const products = [
-    {
-      id: '1',
-      name: '貓咪罐罐',
-      img: 'https://picsum.photos/300/300?text=1',
-      price: 100,
-      quantity: 2,
-    },
-    {
-      id: '2',
-      name: '貓咪干干',
-      img: 'https://picsum.photos/300/300?text=2',
-      price: 200,
-      quantity: 1,
-    },
-  ];
+const ListItem = (props) => {
+  const { id, name, img, price, quantity } = props;
 
-  const list = []
-
-  for (const [i, product] of products.entries()) {
-    list.push(<div className="mb-8 flex justify-between" key={product.id}>
-      <img alt="" src={product.img} className='w-[100px] h-[100px]' />
+  return (
+    <div className="mb-8 flex justify-between" key={id}>
+      <img alt="" src={img} className='w-[100px] h-[100px]' />
       <div className="items-center min-w-[100px]">
-        <span>{product.name}</span>
+        <span>{name}</span>
         <div className="flex justify-between mt-4">
           <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">-</button>
-          <span>{product.quantity}</span>
+          <span>{quantity}</span>
           <button className="w-6 h-6 rounded-3xl bg-[#f0f0f5]">+</button>
         </div>
       </div >
-      <div>$ {product.price}</div>
-    </div>)
-  }
-
-  return (
-    <>
-      {list}
-    </>
+      <div>$ {price}</div>
+    </div>
   )
 }
 
--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from "../../context/CartContext";
 import ListItem from '../Cart/Lineitem';
 
-const Cart = React.memo(() => {
+const Cart = React.memo((props) => {
+  const data = useContext(CartContext);
+
+  const listItem = data.cartItems.map((item) => {
+    return (
+      <ListItem
+        key={item.id}
+        id={item.id}
+        name={item.name}
+        img={item.img}
+        price={item.price}
+        quantity={item.quantity}
+      />
+    );
+  });
+
   return (
     <div className="container py-8 px-6 border divide-[#f0f0f5] rounded-lg">
       <h2 className="mb-8">購物籃</h2>
-      <ListItem />
+      <>{listItem}</>
       <div className="pt-5 pb-8 my-4 flex justify-between">
         <span>運費</span>
         <span>$ 500</span>
