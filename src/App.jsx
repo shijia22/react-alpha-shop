1617488867b0825d838282f6365102ed91cae6e9
shijia22 <shichia0207@gmail.com>
2022-08-11 22:26:49 +0800

    refactor: 💡 移除無用 code

--- src/App.jsx
+++ src/App.jsx
@@ -14,20 +14,6 @@ import cartItems from "./data/items";
 
 const App = () => {
   const [step, setStep] = React.useState(1);
-  // const [cartItems] = useState([{
-  //   id: "1",
-  //   name: "貓咪罐罐",
-  //   img: "https://picsum.photos/300/300?text=1",
-  //   price: 100,
-  //   quantity: 2,
-  // },
-  // {
-  //   id: "2",
-  //   name: "貓咪干干",
-  //   img: "https://picsum.photos/300/300?text=2",
-  //   price: 200,
-  //   quantity: 1,
-  // }]);
 
   const PrevStep = () => {
     if (step === 1) return;
